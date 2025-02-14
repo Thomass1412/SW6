2176b1b02abe3f3d69621b361b0f4bb782af2ad8
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-02-14 12:02:33 +0100

    bug fixes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,38 +1,26 @@
 const express = require("express");
-const { authTest } = require("../controllers/authController");
-const { verifyToken } = require("../middlewares/authMiddleware");
-import { auth } from 'firebase-admin';
-import User from '../models/userModel.js';
-import mongoose from 'mongoose';
-
 const router = express.Router();
-
-// Root Endpoint for API Status
-router.get("/", (req, res) => {
-    res.send("API is running!");
-});
-
-// Protected Endpoint
-router.get("/protected", verifyToken, authTest);
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 
 router.post('/signup', async (req, res) => {
     const { name, email, password, role} = req.body;
-    const userExists = await User.findOne({ email }); 
-    if (userExists) {
-        res.status(400);
-        throw new Error('User already exists');
-    } 
     try {
+        const userExists = await User.findOne({ email });  
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ name, email, password: hashedPassword, role });
         await newUser.save();
+
         res.status(201).json({ message: "User registered successfully", user: newUser });
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
-
-    res.status(201).json(newUser);
 });
 
 router.post('/login', async (req, res) => {
