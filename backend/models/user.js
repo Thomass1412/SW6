f388c0713742028859babd07f882f72c81f13a63
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-02-14 09:07:41 +0100

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["Admin", "User"], default: "User" },
-});
+}, { timestamps: true });
 
 exports.User = mongoose.model('User', userSchema);
