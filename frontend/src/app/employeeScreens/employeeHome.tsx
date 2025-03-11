28d7b944ff35a3949b2a8fa50f7c2c4dcb8332fc
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-11 17:48:34 +0100

    hjb

--- frontend/src/app/employeeScreens/employeeHome.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function EmployeeHome() {
-  return (
-    <View>
-      <Text>Welcome to the Employee Dashboard</Text>
-    </View>
-  );
-}
