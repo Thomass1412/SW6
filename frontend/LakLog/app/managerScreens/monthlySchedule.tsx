c2602532c2b98223036ff096f1a191c2f51f27d9
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-12 11:14:44 +0100

    nav bar works now

--- frontend/LakLog/app/managerScreens/monthlySchedule.tsx
+++ frontend/LakLog/app/managerScreens/monthlySchedule.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 
 export default function ManagerMonthlySchedule() {
+  const handleLogout = async () => {
+    await AsyncStorage.clear();
+    router.replace("/");
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Manager Monthly Schedule</Text>
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 }
