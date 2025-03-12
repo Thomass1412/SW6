ce445b65e5cf3242ee78c486a344156aadc0eb61
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-12 11:53:55 +0100

    styling

--- frontend/LakLog/app/managerScreens/_layout.tsx
+++ frontend/LakLog/app/managerScreens/_layout.tsx
@@ -3,7 +3,17 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function ManagerLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: '#090908', borderTopColor: '#e0e0e0' },
+        tabBarLabelStyle: { fontSize: 12, fontWeight: 'regular', color: '#F7CB8C' },
+        tabBarIconStyle: { marginBottom: -3 },
+        tabBarActiveTintColor: '#F7CB8C',
+        tabBarInactiveTintColor: '#F7CB8C',
+        tabBarActiveBackgroundColor: '#2e2e2e', 
+      }}
+    >
       <Tabs.Screen
         name="monthlySchedule"
         options={{
