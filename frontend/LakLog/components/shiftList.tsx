871a19b1bdb2d30728d2e0a81d1958df8dbd9bec
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-18 12:55:50 +0100

    sorted shifts

--- frontend/LakLog/components/shiftList.tsx
+++ frontend/LakLog/components/shiftList.tsx
@@ -1,16 +1,21 @@
 import { View, FlatList } from "react-native";
 import ShiftCard from "./shiftCard";
-import { Shift } from "../types"; // Import the Shift type
+import { Shift } from "../types";
 
 interface ShiftListProps {
   shifts: Shift[];
 }
 
 const ShiftList: React.FC<ShiftListProps> = ({ shifts }) => {
+  // Sort shifts by startTime (ascending)
+  const sortedShifts = [...shifts].sort((a, b) => {
+    return a.startTime.localeCompare(b.startTime);
+  });
+
   return (
     <View>
       <FlatList
-        data={shifts}
+        data={sortedShifts}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => <ShiftCard shift={item} />}
       />
