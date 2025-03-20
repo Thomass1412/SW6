0cae3fc0dbab9950bb5312b5e78e113e1ce2b395
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-20 11:29:01 +0100

    position prop added

--- frontend/LakLog/components/CustomButton.tsx
+++ frontend/LakLog/components/CustomButton.tsx
@@ -6,12 +6,19 @@ interface CustomButtonProps {
   onPress: () => void;
   iconName: keyof typeof Ionicons.glyphMap;
   text: string;
-  style?: object;
+  position?: { bottom?: number; right?: number };
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ onPress, iconName, text, style }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ onPress, iconName, text, position }) => {
   return (
-    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+    <TouchableOpacity 
+      style={[
+        styles.button, 
+        styles.floatingButton, 
+        position && { bottom: position.bottom, right: position.right }
+      ]} 
+      onPress={onPress}
+    >
       <Ionicons name={iconName} size={30} color="#000" />
       <Text style={styles.buttonText}>{text}</Text>
     </TouchableOpacity>
@@ -30,6 +37,22 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     elevation: 5,
     borderWidth: 2,
+    borderColor: '#000',
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 30,
+    right: 30,  
+    backgroundColor: '#F7CB8C',
+    padding: 15,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 2, 
     borderColor: '#000',
   },
   buttonText: {
