8c8e25d6e2d0a0f452e4183b3dd560db190dce3e
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-20 14:47:15 +0100

    Update createShift.tsx

--- frontend/LakLog/app/admin/createShift.tsx
+++ frontend/LakLog/app/admin/createShift.tsx
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFFAE8",
   },
   label: {
     fontSize: 16,
