9372271a7ceace56a529d55b594f8c9824299de6
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-03-20 11:16:44 +0100

    Update shiftRoutes.js

--- backend/routes/shiftRoutes.js
+++ backend/routes/shiftRoutes.js
@@ -37,7 +37,7 @@ router.get("/all-date", verifyToken, checkAdmin, async (req, res) => {
         }).populate("employee", "name email");
 
         if (shifts.length === 0) {
-            return res.status(404).json({ error: "No shifts found for this date" });
+            return res.status(200).json([]); 
         }
 
         res.json(shifts);
