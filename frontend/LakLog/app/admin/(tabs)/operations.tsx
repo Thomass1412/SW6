323a88a9a3a6e2d6e1777d5e19124a52be0e5dbc
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-04-07 16:06:27 +0200

    Update operations.tsx

--- frontend/LakLog/app/admin/(tabs)/operations.tsx
+++ frontend/LakLog/app/admin/(tabs)/operations.tsx
@@ -1,9 +1,76 @@
-import { View, Text } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function Operations() {
+export default function OperationsScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Operations",
+      headerStyle: {
+        height: 80,
+        backgroundColor: '#F7CB8C',
+      },
+      headerTitleAlign: "center",
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        fontSize: 25,
+      },
+    });
+  }, [navigation]);
+
+  const handlePress = (page: string) => {
+    Alert.alert(`This takes you to the ${page} page`);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Operations</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={() => handlePress("Employees")}>
+        <Text style={styles.buttonText}>Employees</Text>
+      </TouchableOpacity>
+
+      <View style={styles.divider} />
+
+      <TouchableOpacity style={styles.button} onPress={() => handlePress("Shift Types")}>
+        <Text style={styles.buttonText}>Shift Types</Text>
+      </TouchableOpacity>
+
+      <View style={styles.divider} />
+
+      <TouchableOpacity style={styles.button} onPress={() => handlePress("Lokations")}>
+        <Text style={styles.buttonText}>Lokations</Text>
+      </TouchableOpacity>
+
+      <View style={styles.divider} />
+
+      <TouchableOpacity style={styles.button} onPress={() => alert("This generates a report")}>
+        <Text style={styles.buttonText}>Generate Report</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFF7E6',
+    flex: 1,
+    paddingTop: 30,
+    alignItems: 'center',
+  },
+  button: {
+    paddingVertical: 12,
+    width: '85%',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  buttonText: {
+    fontSize: 23,
+    color: '#000',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#ccc',
+    width: '85%',
+  },
+});
