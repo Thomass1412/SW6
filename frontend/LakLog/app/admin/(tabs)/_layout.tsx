340236abf0d52649208684bfce2943405d6cfbdc
Thomas1412 <116066531+Thomass1412@users.noreply.github.com>
2025-05-10 20:07:57 +0200

    Update _layout.tsx

--- frontend/LakLog/app/admin/(tabs)/_layout.tsx
+++ frontend/LakLog/app/admin/(tabs)/_layout.tsx
@@ -6,21 +6,21 @@ export default function AdminTabsLayout() {
     <Tabs
       screenOptions={{
         tabBarStyle: {
-          backgroundColor: "#090908", // Dark background
-          borderTopWidth: 0, // Optional: Removes top border
-          height: 70, // Adjust the height as needed
+          backgroundColor: "#090908", 
+          borderTopWidth: 0, 
+          height: 70, 
         },
-        tabBarActiveBackgroundColor: "#1D1D1D", // Dark background
-        tabBarActiveTintColor: "#F7CB8C", // Slightly grayish active tab
-        tabBarInactiveTintColor: "#F7CB8C", // Icon color
+        tabBarActiveBackgroundColor: "#1D1D1D", 
+        tabBarActiveTintColor: "#F7CB8C", 
+        tabBarInactiveTintColor: "#F7CB8C", 
         tabBarIconStyle: {
-          alignItems: 'center', // Center the icons horizontally
-          justifyContent: 'center', // Center the icons vertically
+          alignItems: 'center',
+          justifyContent: 'center', 
         },
         tabBarLabelStyle: {
-          fontSize: 16, // Increase font size
-          textAlign: 'center', // Center the text horizontally
-          marginTop: 5, // Adjust the margin to bring down the text
+          fontSize: 16, 
+          textAlign: 'center', 
+          marginTop: 5, 
         },
       }}
     >
@@ -29,7 +29,7 @@ export default function AdminTabsLayout() {
         options={{
           title: "Monthly Schedule",
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="calendar-outline" size={size + 5} color={color} /> // Increase icon size
+            <Ionicons name="calendar-outline" size={size + 5} color={color} /> 
           ),
         }}
       />
@@ -38,7 +38,7 @@ export default function AdminTabsLayout() {
         options={{
           title: "Operations",
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="cog-outline" size={size + 5} color={color} /> // Increase icon size
+            <Ionicons name="cog-outline" size={size + 5} color={color} /> 
           ),
         }}
       />
